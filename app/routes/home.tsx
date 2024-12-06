1a8ad5af3f71d5e1364b4bd6ad8a6b666505074e
Pavel Šťastný <stastnyp@seznam.cz>
2024-12-06 21:58:26 +0100

    format - biome.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,17 +1,17 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Welcome } from '../welcome/welcome';
+import type { Route } from './+types/home';
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+	return [
+		{ title: 'New React Router App' },
+		{ name: 'description', content: 'Welcome to React Router!' },
+	];
 }
 
 export function loader({ context }: Route.LoaderArgs) {
-  return { message: context.VALUE_FROM_NETLIFY };
+	return { message: context.VALUE_FROM_NETLIFY };
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
-  return <Welcome message={loaderData.message} />;
+	return <Welcome message={loaderData.message} />;
 }
